a63017957317ba6ae82c2e0c6c5af30310d2d4ea
Russell Schmidt <russell.schmidt@me.com>
2015-11-18 21:24:30 -0800

    add member, collection to question controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
   resources :advertisements, only: [:index, :show, :new, :create]
-  resources :questions
+  resources :questions do
+    collection do
+      get 'seek'
+    end
+    member do
+      get 'asked'
+    end
+  end
+
   resources :topics do
     resources :posts, except: [:index]
   end
 
   resources :users, only: [:new, :create]
-
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
