20a8f21a7b6c60365c08658d1bd32afb677d1c72
Russell Schmidt <russell.schmidt@me.com>
2015-12-07 22:51:17 -0800

    done checkpoint 48

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
     end
   end
 
