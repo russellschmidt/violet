c1b86891aa731e81203450fdac13e95cc21d6c23
Russell Schmidt <russell.schmidt@me.com>
2015-12-09 13:53:41 -0800

    commit to get this out of the way and clone

--- db/schema.rb
+++ db/schema.rb
@@ -78,17 +78,6 @@ ActiveRecord::Schema.define(version: 20151121033553) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
